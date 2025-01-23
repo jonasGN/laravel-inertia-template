8066f969f77a54e7c3bc91542412786c68a992a4
jonasGN <j.goncalvesneto@outlook.com>
2025-01-23 19:01:24 -0300

    inertia instalado e configurado

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
 import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
+        vue(),
         tailwindcss(),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
